36d31fc9e7048eb9f8f3d308edebb182c4e8214f
Kyle DuBois <kydubois28@gmail.com>
2023-12-04 08:14:58 -0800

    add sources

---
+++ snowflake/models/analytics/stg/strip/stg_payments/stg_payments.sql
@@ -0,0 +1,22 @@
+{{
+  config(
+    materialized='table',
+    schema='stg'
+  )
+}}
+
+with payments as (
+
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        -- amount is stored in cents, we convert to dollars
+        amount / 100 as amount, 
+        created as created_at
+    from {{ source('stripe', 'payment') }}
+
+)
+
+select * from payments
